3e40c11d286026a2bbba975e82155143b4877f3f
daisyinb612 <2452825107@qq.com>
2024-08-17 21:41:42 +0800

    fix

--- front-end/src/components/GCharacter.vue
+++ front-end/src/components/GCharacter.vue
@@ -79,7 +79,8 @@
         <el-form-item label="音色">
           <el-select v-model="newAsset.per" placeholder="请选择角色音色">
             <el-option v-for="audio in audios" :key="audio" :label="audio" :value="audio" />
-        </el-form-item>
+         </el-select>
+       </el-form-item>
         
         <el-form-item v-if="newAsset.group === 'characters'" label="图片" :label-width="formLabelWidth">
           <div>
