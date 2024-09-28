f552178f3b4dbca8649b8c46796dd53a2e8f042a
Mars160 <wapollo9@gmail.com>
2024-09-28 11:24:01 +0800

    change WebHistory to WebHashHistory

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,78 +1,77 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '../views/homepage.vue'
-import InputLogLine from '../views/InputLogLine.vue'
-import PageOne from '../views/pageone.vue'
-import PageTwo from '../views/pagetwo.vue'
-import PageThree from '../views/pagethree.vue'
-import PageFour from '../views/pagefour.vue'
-import RenderScript from '../views/renderscript.vue'
-
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomePage from "../views/homepage.vue";
+import InputLogLine from "../views/InputLogLine.vue";
+import PageOne from "../views/pageone.vue";
+import PageTwo from "../views/pagetwo.vue";
+import PageThree from "../views/pagethree.vue";
+import PageFour from "../views/pagefour.vue";
+import RenderScript from "../views/renderscript.vue";
 
 const routes = [
   {
-    path: '/',
-    redirect: '/home'
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/home',
+    path: "/home",
     component: HomePage,
     children: [
       {
-        path: 'inputlogline',
-        name: 'LogLine',
+        path: "inputlogline",
+        name: "LogLine",
         component: InputLogLine,
-        meta:{
-          keepAlive: true
-        }
+        meta: {
+          keepAlive: true,
+        },
       },
       {
-        path: 'pageone',
-        name: 'PageOne',
+        path: "pageone",
+        name: "PageOne",
         component: PageOne,
-        meta:{
-          keepAlive: true
-        }
+        meta: {
+          keepAlive: true,
+        },
       },
       {
-        path: 'pagetwo',
-        name: 'PageTwo',
+        path: "pagetwo",
+        name: "PageTwo",
         component: PageTwo,
-        meta:{
-          keepAlive: true
-        }
+        meta: {
+          keepAlive: true,
+        },
       },
       {
-        path: 'pagethree',
-        name: 'PageThree',
+        path: "pagethree",
+        name: "PageThree",
         component: PageThree,
-        meta:{
-          keepAlive: true
-        }
+        meta: {
+          keepAlive: true,
+        },
       },
       {
-        path: 'pagefour',
-        name: 'PageFour',
+        path: "pagefour",
+        name: "PageFour",
         component: PageFour,
-        meta:{
-          keepAlive: true
-        }
+        meta: {
+          keepAlive: true,
+        },
       },
       {
-        path: 'renderscript',
-        name: 'RenderScript',
+        path: "renderscript",
+        name: "RenderScript",
         component: RenderScript,
-        meta:{
-          keepAlive: true
-        }
-      }
-    ]
-  }
-]
+        meta: {
+          keepAlive: true,
+        },
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   // eslint-disable-next-line no-undef
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
